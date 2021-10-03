8cb68fd4dccabcedef501d9723fd236dcf52ab97
btramme0 <60186776+btramme0@users.noreply.github.com>
2021-10-03 18:43:29 -0400

    Update namelist.js

--- scripts/namelist.js
+++ scripts/namelist.js
@@ -1,7 +1,7 @@
-var firstName = document.getElementById('first_name'); 
+var firstName = document.getElementById("first_name"); 
 	if (firstName == "") {
 		response (firstName); 
 	}
-var lastName = document.getElementById('last_name');
-var middleinitial document.getElementById('middle_initial');
+var lastName = document.getElementById("last_name");
+var middleinitial document.getElementById("middle_initial");
 
