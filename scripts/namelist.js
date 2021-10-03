10fe5bd622fd1b06d0a90c4e73968f2409324bad
btramme0 <60186776+btramme0@users.noreply.github.com>
2021-10-03 18:25:46 -0400

    Update namelist.js

--- scripts/namelist.js
+++ scripts/namelist.js
@@ -1,6 +1,6 @@
 var firstName = document.getElementById('first_name'); 
-	if (firstName => 1) {
-		return firstName; 
+	if (firstName = "") {
+		response (firstName); 
 var lastName = document.getElementById('last_name');
 var middleinitial document.getElementById('middle_initial');
 
