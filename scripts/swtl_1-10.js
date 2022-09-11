5f2c2625ff8e5562c634f21cdd9a97ff33fa1589
btramme0 <60186776+btramme0@users.noreply.github.com>
2022-09-11 18:02:54 -0400

    Add files via upload

---
+++ scripts/swtl_1-10.js
@@ -0,0 +1,29 @@
+
+alert("Hello There and welcome to First Page Books.");
+
+var word1 = "Please ";
+var word2 = "Enjoy!";
+
+
+var sum = "12";
+var sum2 = "1000";
+var unknown = sum + sum2;
+
+var nonLetterorNumber = _$;
+
+var total = 3 * 5 + 5 / 2 % 2;
+
+var newNum = total--;
+
+var num = 25;
+var num2 = 33;
+var num3 = 11;
+var newTotal = (num - num2) * num3;
+
+alert(word1 + word2);
+
+var who = prompt("Who are you?", "Anonymous");
+
+if(prompt === "Anonymous") {
+    alert("unknown it is then.")
+};
