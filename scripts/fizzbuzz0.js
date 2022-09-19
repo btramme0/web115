81b3cff6570e691c1a1d1c0a0bda7e9ffa022c03
btramme0 <60186776+btramme0@users.noreply.github.com>
2022-09-19 03:44:19 -0400

    Add files via upload

--- scripts/fizzbuzz0.js
+++ scripts/fizzbuzz0.js
@@ -1,5 +1,12 @@
-var name = document.getElementById(first_name);
-var name2 = document.getElementById(last_name);
-var name3 = document.getElementById(middle_initail);
+function username()
+  let fname = document.getElementById("first_name");
+  let mname = document.getElementById("last_name");
+  var lname = document.getElementById("middle_initail");
+  var welcome = (`Hello${fname}${mname}${lname}`)
+  document.getElementById("greeting").innerHtml = welcome;
 
-var greeting = alert(`Hello ${name}${name2}${name3}`);
+
+for (var i = 1; i < 125; i++);
+  var counting = "";
+  if (i % 3 == 0) {counting + "New book "}
+  if (i % 5 == 0) {counting + "New  reading"}
