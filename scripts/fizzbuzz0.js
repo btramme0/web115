93d6c63cf07e6f383a3c810cfd4b9b9550e4f8cb
btramme0 <60186776+btramme0@users.noreply.github.com>
2022-10-16 19:23:46 -0400

    Add files via upload

--- scripts/fizzbuzz0.js
+++ scripts/fizzbuzz0.js
@@ -1,9 +1,10 @@
-function username()
+function username() {
   var fname = document.getElementById("first_name");
   var mname = document.getElementById("last_name");
   var lname = document.getElementById("middle_initail");
   var welcome = (`Hello${fname}${mname}${lname}`)
   document.getElementById("greeting").innerHTML = "welcome";
+}
 
 var question = prompt("How high would you like to count?");
 for (var i = 1; i < 125; i++);
