e66d8d875667976bf3598c88ba0e18c2ea697b9e
btramme0 <60186776+btramme0@users.noreply.github.com>
2022-11-08 20:20:51 -0500

    Add files via upload

--- fizzbuzz0.js
+++ fizzbuzz0.js
@@ -2,15 +2,18 @@ function username() {
   var fname = document.getElementById("first_name").value;
   var mname = document.getElementById("last_name").value;
   var lname = document.getElementById("middle_initail").value;
-  alert = ("Hello " + "" + fname+ "" + mname + "" + lname);
+  alert("Hello " + " " + fname+ " " + mname + " " + lname);
   var question = prompt("How high would you like to count?");
-  document.getElementById("greeting").innerHTML = "welcome";
-}
-
-function header() { 
-for (var i = 1; i < 125; i++);
+  for (var i = 1; i <= 125; i++);
   var counting = "";
   if (i % 3 == 0) {`${counting}New book`}
   if (i % 5 == 0) {`${counting}New  reading`}
   document.getElementById("greeting").innerHTML = "welcome";
+}
+
+function header() { 
+  var fname = document.getElementById("first_name").value;
+  var mname = document.getElementById("last_name").value;
+  var lname = document.getElementById("middle_initail").value
+  document.getElementById("greeting").innerHTML = "welcome " + fname + " " + mname + " " + lname;
 };
